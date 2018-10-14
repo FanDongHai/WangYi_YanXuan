8768d8ef5261c4f6b913968334b9c06c147e697f
FanDongHai <1391933854@qq.com>
2018-10-14 17:00:50 +0800

    更新

--- src/main/java/com/wangyi/wangyi_yanxuan/sfd/hello.java
+++ src/main/java/com/wangyi/wangyi_yanxuan/sfd/hello.java
@@ -2,6 +2,6 @@ package com.wangyi.wangyi_yanxuan.sfd;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println(243423423);
+        System.out.println(2);
     }
 }
