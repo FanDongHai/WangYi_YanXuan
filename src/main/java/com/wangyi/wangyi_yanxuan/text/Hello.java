5b1f70040720a0bd1ea1fc3c94a6eb2134a0949d
FanDongHai <1391933854@qq.com>
2018-10-14 16:53:41 +0800

    更新

--- src/main/java/com/wangyi/wangyi_yanxuan/text/Hello.java
+++ src/main/java/com/wangyi/wangyi_yanxuan/text/Hello.java
@@ -2,6 +2,6 @@ package com.wangyi.wangyi_yanxuan.text;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println(1456);
+        System.out.println(14446);
     }
 }
