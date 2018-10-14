f524ac9c07c04c3cbfa66a86b80161c19d274168
renr <123444@aliyun.com>
2018-10-14 17:07:48 +0800

    更新

--- src/main/java/com/wangyi/wangyi_yanxuan/sf/h.java
+++ src/main/java/com/wangyi/wangyi_yanxuan/sf/h.java
@@ -3,5 +3,14 @@ package com.wangyi.wangyi_yanxuan.sf;
 public class h {
     public static void main(String[] args) {
         System.out.println(4444);
+
+        System.out.println(8888);
     }
+
+
+
 }
+
+
+
+
