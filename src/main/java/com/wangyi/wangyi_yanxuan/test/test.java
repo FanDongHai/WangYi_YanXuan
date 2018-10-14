e58f228d891f2457cbcbfd602675d0c7947712fa
renr <123444@aliyun.com>
2018-10-14 17:12:12 +0800

    更新

--- src/main/java/com/wangyi/wangyi_yanxuan/test/test.java
+++ src/main/java/com/wangyi/wangyi_yanxuan/test/test.java
@@ -2,6 +2,6 @@ package com.wangyi.wangyi_yanxuan.test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println(123);
+        System.out.println(12);
     }
 }
