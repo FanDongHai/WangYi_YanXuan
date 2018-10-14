ef575d4866ac2c8c969da9b6aefa7e7745ac801f
renr <123444@aliyun.com>
2018-10-14 18:57:48 +0800

    fsfs

--- src/main/java/com/wangyi/wangyi_yanxuan/text/hello.java
+++ src/main/java/com/wangyi/wangyi_yanxuan/text/hello.java
@@ -3,5 +3,6 @@ package com.wangyi.wangyi_yanxuan.text;
 public class hello {
     public static void main(String[] args) {
         System.out.println(1324);
+        System.out.println(8888);
     }
 }
