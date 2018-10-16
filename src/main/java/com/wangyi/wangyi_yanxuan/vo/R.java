e998c74ff908054f001eff79a2172d8cdd684d08
FanDongHai <1391933854@qq.com>
2018-10-16 21:39:35 +0800

    修改

--- src/main/java/com/wangyi/wangyi_yanxuan/vo/R.java
+++ src/main/java/com/wangyi/wangyi_yanxuan/vo/R.java
@@ -1,23 +1,20 @@
 package com.wangyi.wangyi_yanxuan.vo;
 
-<<<<<<< HEAD
-=======
+
 /**
  *@Author feri
  *@Date Created in 2018/10/12 11:36
  */
->>>>>>> origin/master
+
 public class R {
     private int code;
     private String msg;
     private Object data;
 
     public int getCode() {
-<<<<<<< HEAD
+
         return this.code;
-=======
-        return code;
->>>>>>> origin/master
+
     }
 
     public void setCode(int code) {
@@ -25,11 +22,9 @@ public class R {
     }
 
     public String getMsg() {
-<<<<<<< HEAD
+
         return this.msg;
-=======
-        return msg;
->>>>>>> origin/master
+
     }
 
     public void setMsg(String msg) {
@@ -37,11 +32,10 @@ public class R {
     }
 
     public Object getData() {
-<<<<<<< HEAD
+
         return this.data;
-=======
-        return data;
->>>>>>> origin/master
+
+
     }
 
     public void setData(Object data) {
@@ -54,10 +48,6 @@ public class R {
         this.data = data;
     }
 
-<<<<<<< HEAD
     public R() { }
-=======
-    public R() {
-    }
->>>>>>> origin/master
+
 }
